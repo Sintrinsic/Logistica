0683295ce78256b540cd53376450c37f87b96503
sintrinsic <brandon@pwns.me>
2019-06-08 17:04:29 -0700

    First commit of refactored mod. Still using all the examplemod's base code. Just changed the name. Tested, and running.

--- src/main/java/me/pwns/logistica/Logistica.java
+++ src/main/java/me/pwns/logistica/Logistica.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package me.pwns.logistica;
 
 import net.minecraft.block.Block;
 //import net.minecraft.block.Blocks;
@@ -19,13 +19,13 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("logistica")
+public class Logistica
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public Logistica() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
