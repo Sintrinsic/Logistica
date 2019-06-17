3ceeabf14a04213f0ffca7426b2ede198dd7fd83
Vaporose <v@pwns.me>
2019-06-16 20:37:23 -0700

    Working queue hash

--- src/main/java/me/pwns/logistica/world/regen/WorldRegenManager.java
+++ src/main/java/me/pwns/logistica/world/regen/WorldRegenManager.java
@@ -1,17 +1,12 @@
 package me.pwns.logistica.world.regen;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
+import java.util.*;
 
 /**
  * TODO complete javadoc
@@ -19,12 +14,15 @@ import java.util.LinkedHashMap;
 @Mod.EventBusSubscriber
 public class WorldRegenManager {
 
+    private static HashMap<BlockState, BlockGroup> destroyedBlockGroup = new HashMap<>();
+    private static HashMap<BlockGroup, Integer> destroyedBlockGroupQueue = new HashMap<>();
+
     /** TODO complete javadoc
      * @param event This is the block event being listened for.
      */
     @SubscribeEvent
     public static void blockDestroyedListener(BlockEvent.BreakEvent event) {
-
+        if (event.getWorld().isRemote()) return;
         HashMap<BlockState, BlockState> blockGroupList = new HashMap<>();
 
         // TODO this is ugly as hell, fix it.
@@ -44,19 +42,40 @@ public class WorldRegenManager {
         blockGroupList.put(event.getState(), brokenBlockState);
 
 
-
         BlockGroup group = new BlockGroup(blockGroupList);
-        System.out.println(group);
+        destroyedBlockGroupQueue.put(group, 1);
 
     }
 
-    // @SubscribeEvent TODO uncomment when ready
-    public static void checkDestroyedQueue(TickEvent.WorldTickEvent tick) {
+    @SubscribeEvent
+    public static void destroyedBlockQueueListener(TickEvent.WorldTickEvent tick) {
+        if (tick.world.isRemote()) return;
+        if (!destroyedBlockGroupQueue.isEmpty()) {
+            System.out.println("DEBUG: Queue not empty. Doing the things.");
+
+            for (Map.Entry<BlockGroup, Integer> entry : destroyedBlockGroupQueue.entrySet()) {
+                System.out.println("For condition met");
+                BlockGroup key = entry.getKey();
+                int value = entry.getValue() + 1;
+                if (value >= 10) {
+                    System.out.println("Value of " + key + "is over 10");
+                    if (checkPlayerPresent(key)) {
+                        destroyedBlockGroupQueue.put(key, 1);
+                    } else { cleanDestroyedBlockGroup(key); }
+                }
+            }
+
+            System.out.println("New queue: " + destroyedBlockGroupQueue);
+        }
 
     }
 
-    private static void cleanDestroyedBlockGroup(BlockState state, String string) {
+    private static boolean checkPlayerPresent(BlockGroup group){
+        return false; // TODO update to be real
+    }
 
+    private static void cleanDestroyedBlockGroup(BlockGroup key) {
+        destroyedBlockGroupQueue.remove(key);
     }
 
 }
