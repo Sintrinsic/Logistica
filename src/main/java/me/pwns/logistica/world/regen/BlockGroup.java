a7d65ba3b2f478ece61a76e656725ebf644f5cdb
Vaporose <v@pwns.me>
2019-06-17 00:27:34 -0700

    * Added javadocs * Added zoneListener to check for PlayerZoneEvents * Improved join method to add new positions to this.zone

--- src/main/java/me/pwns/logistica/world/regen/BlockGroup.java
+++ src/main/java/me/pwns/logistica/world/regen/BlockGroup.java
@@ -1,8 +1,12 @@
 package me.pwns.logistica.world.regen;
 
+import me.pwns.logistica.events.PlayerZoneEvent;
+import me.pwns.logistica.events.ZoneEventType;
+import me.pwns.logistica.util.zones.BlockGroupZone;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -10,34 +14,73 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 
+/**
+ * Groups together adjacent blocks by their current BlockState (see SavedBlockState, in order to  facilitate replacing a
+ * specific BlockPos with the previous BlockState
+ */
 public class BlockGroup {
     private HashMap<BlockPos, SavedBlockState> blockStateList = new HashMap<>();
     private boolean hasBeenRestored = false;
-    private int lastTouched;
+    private float lastTouched;
+    private BlockGroupZone zone;
+    private World world;
+    private boolean isOccupied;
 
-    public BlockGroup(HashMap<BlockPos, SavedBlockState> blockArray) {
-        this.blockStateList.putAll(blockArray);
-    }
 
+    /** Constructor.
+     * @param block this is a SavedBlockState that has World, BlockPos, and BlockState as arguments
+     */
     public BlockGroup(SavedBlockState block) {
         addBlock(block);
+        this.world = block.getWorld(); // Although other blocks are added later, shouldn't ever be jumping worlds.
+        this.zone = new BlockGroupZone(blockStateList.keySet(), block.getWorld());
     }
 
+    /**
+     * @param group Another Blockgroup. Joins another blockgroup whenever two groups interact.
+     * @return "this," to be explicit which group is being kept.
+     */
     public BlockGroup joinGroup(BlockGroup group) {
-        this.blockStateList.putAll(group.blockStateList);
+        this.blockStateList.putAll(group.blockStateList); // Explicit "this" for clarity's sake.
+        group.blockStateList.forEach((pos, savedState) -> zone.addBlock(pos));  // Add the blocks to the zone.
         return this;
     }
 
+    /** Adds a block to the block list.
+     * @param block SavedBlockState block.
+     */
     private void addBlock(SavedBlockState block) {
         blockStateList.put(block.getPos(), block);
+        zone.addBlock(block.getPos());
     }
 
+    /**
+     * Called whenever the blockgroup needs to be restored to its previous state.
+     */
     public void restore() {
         for(Map.Entry<BlockPos, SavedBlockState> entry: blockStateList.entrySet()) {
             SavedBlockState savedState = entry.getValue();
             savedState.getWorld().setBlockState(savedState.getPos(), savedState.getState());
-            hasBeenRestored = true;
+            hasBeenRestored = true; // Currently not used, included for future proofing.
         };
     }
+
+    /**
+     * @param event Listens to PlayerZoneEvents for players exiting and entering zone
+     */
+    @SubscribeEvent
+    public void zoneListener(PlayerZoneEvent event) {
+        if (event.getPlayer().getEntityWorld().isRemote()) return;
+        if (event.getZone() != zone) return;
+        if (event.getEventType() == ZoneEventType.ENTERING) {
+            lastTouched = world.getGameTime();
+            isOccupied = true;
+        }
+        else if (event.getEventType() == ZoneEventType.EXITING) {
+            // Whenever a player leaves a zone, WorldRegenManager will start watching and look at lastTouched
+            isOccupied = false;
+        }
+    }
+
 
 }
