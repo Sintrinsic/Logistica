3112d5238c9994b6ac03f9d8aca93e944b9a6527
Sintrinsic <sintrinsicx@hotmail.com>
2019-06-18 22:13:50 -0700

    Adding basic callback functionality to unblock V.

---
+++ src/main/java/me/pwns/logistica/util/time/callbacks/CallbackContainer.java
@@ -0,0 +1,9 @@
+package me.pwns.logistica.util.time.callbacks;
+
+
+/**
+ * Interface to allow for the passing of arbitrary callbacks or remotely executed logic.
+ */
+public interface CallbackContainer {
+    void doCallback();
+}
